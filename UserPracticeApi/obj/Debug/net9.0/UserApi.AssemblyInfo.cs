d04ab765daae6f0df0cfaa9195f9d1b4e2ae5249
Javier Garcia <aj.garcia20@info.uas.edu.mx>
2025-03-02 14:00:04 -0700

    ajuste directorio

--- UserPracticeApi/obj/Debug/net9.0/UserApi.AssemblyInfo.cs
+++ UserPracticeApi/obj/Debug/net9.0/UserApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UserApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cdd2979b72924f419bb95009c8ba121ba2fe0024")]
 [assembly: System.Reflection.AssemblyProductAttribute("UserApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UserApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
